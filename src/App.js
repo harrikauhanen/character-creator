66b63666eb0eead83b789602b0176926fc63bd79
Harri Kauhanen <harri.kauhanen@humaneq.com>
2022-01-25 18:55:38 +0200

    Exercise 4: Perspective decoration
    Probably was not meant to add new DOM elements, but did not find anything to hook into. Using inline styles for the victory.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <div
+        style={{
+          backgroundColor: 'hsl(195deg, 20%, 86%)',
+          position: 'fixed',
+          width: '100%',
+          height: '40%',
+          bottom: '0px',
+        }}
+      ></div>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
